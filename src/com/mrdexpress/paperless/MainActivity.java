d07c356f6a80147fb1147e8f95b9c4fc5590168d
Heinrich Dahms <htdahms@gmail.com>
2014-02-19 09:29:39 +0200

    Added some method stubs required for GCM.

--- src/com/mrdexpress/paperless/MainActivity.java
+++ src/com/mrdexpress/paperless/MainActivity.java
@@ -1,13 +1,18 @@
 package com.mrdexpress.paperless;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -23,6 +28,9 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.mrdexpress.paperless.adapters.UserAutoCompleteAdapter;
 import com.mrdexpress.paperless.datatype.UserItem;
 import com.mrdexpress.paperless.datatype.UserItem.UserType;
@@ -38,11 +46,22 @@ public class MainActivity extends Activity
 	private ViewHolder holder;
 	private View root_view;
 	private final String TAG = "MainActivity";
-	ArrayList<UserItem> person_item_list;
+	private ArrayList<UserItem> person_item_list;
 	private String selected_user_id;
 	private String selected_user_name;
 	private UserType selected_user_type;
-	String imei_id;
+	private String imei_id;
+
+	public static final String EXTRA_MESSAGE = "message";
+	public static final String PROPERTY_REG_ID = "registration_id";
+	private static final String PROPERTY_APP_VERSION = "appVersion";
+	private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+	private String SENDER_ID = "Your-Sender-ID";
+	private GoogleCloudMessaging gcm;
+	private String regid;
+	AtomicInteger msgId = new AtomicInteger();
+	Context context;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -63,8 +82,26 @@ public class MainActivity extends Activity
 
 		// Set global variable holding context
 		VariableManager.context = this;
+		context = getApplicationContext();
 
 		setTitle(R.string.title_actionbar_mainmenu); // Change actionbar title
+
+		// Check device for Play Services APK. If check succeeds, proceed with
+		// GCM registration.
+		if (checkPlayServices())
+		{
+			gcm = GoogleCloudMessaging.getInstance(this);
+			regid = getRegistrationId(context);
+
+			if (regid.isEmpty())
+			{
+				registerInBackground();
+			}
+		}
+		else
+		{
+			Log.i(TAG, "No valid Google Play Services APK found.");
+		}
 
 		person_item_list = new ArrayList<UserItem>();
 
@@ -100,6 +137,16 @@ public class MainActivity extends Activity
 			}
 		});
 
+	}
+
+	/**
+	 * Registers the application with GCM servers asynchronously.
+	 * <p>
+	 * Stores the registration ID and app versionCode in the application's shared preferences.
+	 */
+	private void registerInBackground()
+	{
+		// TODO: Add AsyncTask that can registers a new id if needed.
 	}
 
 	/**
@@ -491,6 +538,89 @@ public class MainActivity extends Activity
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	/**
+	 * Check the device to make sure it has the Google Play Services APK. If
+	 * it doesn't, display a dialog that allows users to download the APK from
+	 * the Google Play Store or enable it in the device's system settings.
+	 */
+	private boolean checkPlayServices()
+	{
+		int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		if (resultCode != ConnectionResult.SUCCESS)
+		{
+			if (GooglePlayServicesUtil.isUserRecoverableError(resultCode))
+			{
+				GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+						PLAY_SERVICES_RESOLUTION_REQUEST).show();
+			}
+			else
+			{
+				Log.i(TAG, "This device is not supported.");
+				finish();
+			}
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Gets the current registration ID for application on GCM service.
+	 * <p>
+	 * If result is empty, the app needs to register.
+	 * 
+	 * @return registration ID, or empty string if there is no existing
+	 *         registration ID.
+	 */
+	private String getRegistrationId(Context context)
+	{
+		final SharedPreferences prefs = getGCMPreferences(context);
+		String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+		if (registrationId.isEmpty())
+		{
+			Log.i(TAG, "Registration not found.");
+			return "";
+		}
+		// Check if app was updated; if so, it must clear the registration ID
+		// since the existing regID is not guaranteed to work with the new
+		// app version.
+		int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+		int currentVersion = getAppVersion(context);
+		if (registeredVersion != currentVersion)
+		{
+			Log.i(TAG, "App version changed.");
+			return "";
+		}
+		return registrationId;
+	}
+
+	/**
+	 * @return Application's {@code SharedPreferences}.
+	 */
+	private SharedPreferences getGCMPreferences(Context context)
+	{
+		// This sample app persists the registration ID in shared preferences, but
+		// how you store the regID in your app is up to you.
+		return getSharedPreferences(MainActivity.class.getSimpleName(), Context.MODE_PRIVATE);
+	}
+
+	/**
+	 * @return Application's version code from the {@code PackageManager}.
+	 */
+	private static int getAppVersion(Context context)
+	{
+		try
+		{
+			PackageInfo packageInfo = context.getPackageManager().getPackageInfo(
+					context.getPackageName(), 0);
+			return packageInfo.versionCode;
+		}
+		catch (NameNotFoundException e)
+		{
+			// should never happen
+			throw new RuntimeException("Could not get package name: " + e);
+		}
 	}
 
 	public void initViewHolder()
