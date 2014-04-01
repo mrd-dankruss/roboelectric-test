5b95811fe9d9b197d76f7b7f9d8a4dec3de310c8
Gary Benade <gary.benade@mrd.com>
2014-04-01 13:02:52 +0200

    fix set next stop

---
+++ src/com/mrdexpress/paperless/datatype/StopItem.java
@@ -0,0 +1,81 @@
+package com.mrdexpress.paperless.datatype;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import com.mrdexpress.paperless.workflow.JSONObjectHelper;
+import com.mrdexpress.paperless.workflow.ObservableJSONObject;
+
+/**
+ * Created by gary on 2014/04/01.
+ */
+public class StopItem implements Parcelable
+{
+    private ObservableJSONObject data;
+
+    public StopItem( ObservableJSONObject stop)
+    {
+        this.data = stop;
+    }
+
+    public StopItem(Parcel in)
+    {
+        readFromParcel(in);
+    }
+
+    public int describeContents()
+    {
+        return 0;
+    }
+
+    public int getID()
+    {
+        return JSONObjectHelper.getIntDef(data.get(), "id", -1);
+    }
+
+    public void setTripOrder( int triporder)
+    {
+        data.get().put("triporder", triporder);
+    }
+
+    public int getTripOrder()
+    {
+        return JSONObjectHelper.getIntDef( data.get(), "triporder", -1);
+    }
+
+    public String getAddress()
+    {
+        return JSONObjectHelper.getStringDef( data.get(), "address", "!");
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return getAddress();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags)
+    {
+        dest.writeString( data.getJSON());
+    }
+
+    public void readFromParcel(Parcel in)
+    {
+        data.setJSON( in.readString());
+    }
+
+    public static final Parcelable.Creator<StopItem> CREATOR = new Parcelable.Creator<StopItem>()
+    {
+        public StopItem createFromParcel(Parcel in)
+        {
+            return new StopItem(in);
+        }
+
+        public StopItem[] newArray(int size)
+        {
+            return new StopItem[size];
+        }
+    };
+}
+
